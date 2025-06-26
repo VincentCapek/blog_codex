22cba000dcdca05d3e6bb07e3bbd51bab3a2a003
Blams-A0mine <31575795+VincentCapek@users.noreply.github.com>
2025-06-26 17:54:17 +0200

    style: customize pagination for light theme

---
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -0,0 +1,52 @@
+@if ($paginator->hasPages())
+    <nav role="navigation" aria-label="Pagination" class="flex justify-center">
+        <ul class="inline-flex items-center -space-x-px">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li>
+                    <span class="px-3 py-2 ml-0 leading-tight text-gray-400 bg-white border border-gray-300 rounded-l-lg">{!! __('pagination.previous') !!}</span>
+                </li>
+            @else
+                <li>
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">{!! __('pagination.previous') !!}</a>
+                </li>
+            @endif
+
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <li>
+                        <span class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300">{{ $element }}</span>
+                    </li>
+                @endif
+
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li>
+                                <span aria-current="page" class="px-3 py-2 leading-tight text-white bg-blue-500 border border-blue-500">{{ $page }}</span>
+                            </li>
+                        @else
+                            <li>
+                                <a href="{{ $url }}" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">{{ $page }}</a>
+                            </li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li>
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">{!! __('pagination.next') !!}</a>
+                </li>
+            @else
+                <li>
+                    <span class="px-3 py-2 leading-tight text-gray-400 bg-white border border-gray-300 rounded-r-lg">{!! __('pagination.next') !!}</span>
+                </li>
+            @endif
+        </ul>
+    </nav>
+@endif
